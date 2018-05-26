212bc6d634939cdf79f846a0468b53b9d68fc1ca
juanjuar457 <testjj4573421@gmail.com>
2018-05-25 19:51:36 -0700

    first upload

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+}
+
+
+// keep in mind what braden said about templateURL
